78aa7bb5117eb83515d205d8b2350b2f70b4d0fd
Humphrey Pietersen <humphrey.pietersen@gmail.com>
2022-12-03 11:31:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php echo header();
-
-$file = file_get_contents("./README.md");
- echo "<pre style=\"word-wrap: break-word; white-space: pre-wrap;\"> ";
- echo $file;
-  echo "</pre>";
+<?= #silence is golden
